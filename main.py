76704e8263360f0f4a5d247ae2599a1b94036ab1
19hasanpes03-cmd <19hasanpes03@gmail.com>
2026-08-09 15:54:49 +0300

    Implement background HTTP server for bot status
    Added a simple HTTP server to run in the background.

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import discord
 from discord.ext import commands
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import threading
 
+# Render'ın web servisi için arka planda port dinleyen mini sunucu
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot 7/24 aktif calisiyor!")
+
+def run_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(('0.0.0.0', port), SimpleHandler)
+    server.serve_forever()
+
+server_thread = threading.Thread(target=run_server)
+server_thread.daemon = True
+server_thread.start()
+
+# Discord Bot Kodları
 intents = discord.Intents.default()
 intents.message_content = True
 intents.guilds = True
